07fc70ac7794b250646132f22e5e690121b95ac8
syed-anus-ali <165845895+syed-anus-ali@users.noreply.github.com>
2024-04-25 01:43:04 +0500

    you have to do

---
+++ main.ts
@@ -0,0 +1,28 @@
+import inquirer from "inquirer"
+
+let toDos = [];
+let condition = true;
+
+
+while(condition)
+{
+    let addTask = await inquirer.prompt(
+    [
+        {
+            name:"todo",
+            type:"input",
+            message:"what do you want to add in toDos ?"
+        },
+
+        {
+           name:"addMore",
+           type:"confirm",
+           message:"are you sure you want to add more todos ?",
+           default:"false"
+        }
+    ]
+);
+toDos.push(addTask.todo);
+condition = addTask.addMore
+console.log(toDos);
+}
